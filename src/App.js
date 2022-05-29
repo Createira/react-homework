aaa679305ee4a045fdc4bcff61f7cee8ed565bae
createira <ira.verbilovich@mail.ru>
2022-05-29 17:05:03 +0300

    feat(16): add solution for task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import Box from "./components/Boxes";
+// import Box from "./components/Boxes";
+import Task from "./components/Task";
 
-function App() {
-  const boxesData = [
-    {
-      cssClass: "item-small",
-      label: "Small Box",
-    },
-    {
-      cssClass: "item-middle",
-      label: "Medium Box",
-    },
-    {
-      cssClass: "item-big",
-      label: "Big Box",
-    },
-  ];
-
+function App({ number, text, isFinished }) {
+  // const boxesData = [
+  //   {
+  //     cssClass: "item-small",
+  //     label: "Small Box",
+  //   },
+  //   {
+  //     cssClass: "item-middle",
+  //     label: "Medium Box",
+  //   },
+  //   {
+  //     cssClass: "item-big",
+  //     label: "Big Box",
+  //   },
+  // ];
+  
   return (
     <div className="main">
-      <Box className={boxesData[0].cssClass} label={boxesData[0].label} />
+      {/* <Box className={boxesData[0].cssClass} label={boxesData[0].label} />
       <Box className={boxesData[1].cssClass} label={boxesData[1].label} />
-      <Box className={boxesData[2].cssClass} label={boxesData[2].label} />
+      <Box className={boxesData[2].cssClass} label={boxesData[2].label} /> */}
+      <Task number={number} text={text} done={isFinished} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import Task from "./components/Task";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Task number={1.} text="Create immaculate final project on React" isFinished={false}/>
+    <Task number={2.} text="Find a new job" isFinished={false}/>
+    <Task number={3.} text="Reach the Upper Intermediate level in English" isFinished={true}/>
+    <Task number={4.} text="Work and earn 10,000$" isFinished={false}/>
+    <Task number={5.} text="Buy a BMW" isFinished={false}/>
   </React.StrictMode>
 );
